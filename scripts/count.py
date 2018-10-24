7f479e05f005e1a07c671a7e8b11419b1f0a305f
tynanseltzer <tynanseltzer@gmail.com>
2018-10-24 03:14:49 -0400

    more data, uncontested

--- scripts/count.py
+++ scripts/count.py
@@ -12,7 +12,7 @@ c = 0
 for year in [2018]:
     for state in states:
         for num in range(55):
-            my_df = df.loc[(df["Cand_Office_St"] == "TX") & (df["Cand_Office_Dist"] == 8) & (df["Year"] == year)]
-            if len(my_df.index) >= 1:
-                print(my_df)
+            my_df = df.loc[(df["Cand_Office_St"] == state) & (df["Cand_Office_Dist"] == num) & (df["Year"] == year)]
+            if len(my_df.index) == 1:
+                my_df.to_csv("../data/uncontested.csv", mode='a', header=False)
 print(c)
