855d20e4156191ffe087616f2aceb1b80975004d
tynanseltzer <tynanseltzer@gmail.com>
2018-10-23 23:06:17 -0400

    appended parties

--- scripts/partify.py
+++ scripts/partify.py
@@ -1,7 +1,8 @@
 import pandas as pd
-candidates = pd.read_csv("../data/winners.csv", engine='python')
+df = pd.read_csv("../data/winners.csv", engine='python')
 
-
+foo = [0]*24
+x = pd.Series(foo)
 states = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DC", "DE", "FL", "GA",
           "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD",
           "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ",
@@ -12,4 +13,10 @@ for year in [2010, 2012, 2014, 2016, 2018]:
     for state in states:
         for num in range(55):
             my_df = df.loc[(df["Cand_Office_St"] == state) & (df["Cand_Office_Dist"] == num) & (df["Year"] == year)]
+
+            if len(my_df) == 2:
+               my_df = my_df.iloc[0].append(my_df.iloc[1]).to_frame().T
+               my_df.to_csv("../data/parties.csv", mode='a', header=False)
+    print(year)
+
 
